25d8afa69c7c61fb1c8ff1ccd68d2adc0533e3d8
Darko <darko.simonovski@bestisoft.com>
2019-05-30 00:58:56 +0200

    Added alternate item basics template
    Added vertical prop (see notes)

--- app/components/blocks/item/ItemBasics.vue
+++ app/components/blocks/item/ItemBasics.vue
@@ -1,20 +1,31 @@
 <template>
-  <FlexboxLayout flexDirection="row" alignItems="center" width="100%">
+  <FlexRow alignItems="center" width="100%" v-if="!vertical">
     <Image :src="item.images[0].url" width="20%"/>
-    <FlexboxLayout flexDirection="column" justifyContent="center" marginLeft="15">
-      <Label :text="item.title" fontSize="14" fontWeight="bold" color="black" padding="3"/>
-      <Label :text="item.category.name" fontSize="14" padding="3"/>
-    </FlexboxLayout>
-  </FlexboxLayout>
+    <FlexCol justifyContent="center" marginLeft="15">
+      <Label :text="item.title" fontWeight="bold" color="black" fontSize="14" padding="3"/>
+      <Label v-if="item.category" :text="item.category.name" fontSize="14" padding="3"/>
+    </FlexCol>
+  </FlexRow>
+  <FlexCol justifyContent="center" v-else>
+    <Image :src="item.images[0].url" width="30%"/>
+    <Split size="2"/>
+    <Label :text="item.title" fontWeight="bold" color="black" fontSize="20" padding="3"/>
+    <Split v-if="item.category" size="2"/>
+    <Label v-if="item.category" :text="item.category.name" fontSize="18" padding="3"/>
+  </FlexCol>
 </template>
 
 <script>
+// do we want this with 'vertical' like this? maybe create another one if needed
+// as in, 'vertical' means more than just orientation :p
+
 export default {
   props: {
     item: {
       type: Object,
       required: true
-    }
+    },
+    vertical: Boolean
   }
 }
 </script>
