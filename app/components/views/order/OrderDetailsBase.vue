897ba83c90e44b03903f739ff38342e51629bd79
Darko <darko.simonovski@bestisoft.com>
2019-05-30 00:59:54 +0200

    Initial version of order details base template
    With mocks

---
+++ app/components/views/order/OrderDetailsBase.vue
@@ -0,0 +1,88 @@
+<template>
+  <ViewContainer :loading="!loaded">
+    <ScrollView orientation="vertical">
+      <FlexCol alignItems="center" justifyContent="space-around" height="100%">
+        <ItemBasics :item="order.item" vertical/>
+        <FlexCol class="feedback" width="100%">
+          <FlexRow justifyContent="space-between" alignItems="center">
+            <Label fontWeight="bold" fontSize="18" text="Feedback"/>
+            <LabelButton text="Leave feedback"/>
+          </FlexRow>
+          <Split small/>
+          <Label text="Let us how your transaction went"/>
+        </FlexCol>
+        <FlexCol class="shipping" width="100%">
+          <FlexRow justifyContent="space-between" alignItems="center">
+            <Label fontWeight="bold" fontSize="18" text="Shipping address"/>
+            <LabelButton text="Resolve shipping issue"/>
+          </FlexRow>
+          <Split small/>
+          <Label :text="order.shippingAddress"/>
+        </FlexCol>
+        <FlexCol class="payment" width="100%">
+          <FlexRow justifyContent="space-between" alignItems="center">
+            <Label fontWeight="bold" fontSize="18" text="Payment"/>
+            <LabelButton text="Ask for refund"/>
+          </FlexRow>
+          <Split small/>
+          <Label text="Payment sent over Stripe"/>
+        </FlexCol>
+        <FlexCol class="charges" width="100%">
+          <DataGrid :data="chargeItems"/>
+        </FlexCol>
+        <FlexCol class="seller" width="100%">
+          <FlexRow justifyContent="space-between" alignItems="center">
+            <Label fontWeight="bold" fontSize="18" text="Seller"/>
+            <LabelButton text="Message seller"/>
+          </FlexRow>
+          <Split small/>
+          <Label :text="'Purchase on ' + order.createdAt + ' from ' + order.buyer.username"/>
+        </FlexCol>
+        <Split full/>
+        <StateButton block text="Mark received"/>
+      </FlexCol>
+    </ScrollView>
+  </ViewContainer>
+</template>
+
+<script>
+import Api from '@/services/api'
+import ItemBasics from '@/components/blocks/item/ItemBasics'
+import mocks from '@/services/mocks'
+
+export default {
+  props: {
+    orderId: {
+      type: Number,
+      required: true
+    }
+  },
+  created() {
+    this.order = mocks.order
+    this.loaded = true
+    return false
+    Api.getOrder(this.orderId)
+      .then((order) => {
+        this.order = order
+        this.loaded = true
+      })
+  },
+  data: () => ({
+    order: {},
+    loaded: false
+  }),
+  computed: {
+    chargeItems() { // rename later
+      return [
+        { label: 'Item price', value: '$2.00' },
+        { label: 'Shipping price', value: 'Free' },
+        { label: 'Total', value: '$2.00' }
+      ]
+    }
+  },
+  components: {
+    ItemBasics
+  }
+}
+
+</script>
